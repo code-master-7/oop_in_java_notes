c622ade1af87abbab74725cdfe6860ce5276d43e
Nimesh <nimeshkadecha4560@gmail.com>
2024-05-02 10:56:28 +0530

    Adding p23

--- Programs/p23.java
+++ Programs/p23.java
@@ -1,25 +1,14 @@
 // Write a program that demonstrates multiple inheritance using interface
 interface circle {
-    int r = 2;
-    void area(int r);
+    default void print(int r){ System.out.println(3.14*r);}
 }
-
 interface square {
-    int l = 5, m = 5;
+    default void print(int l,int m){ System.out.println(l*m); }
 }
-
 public class p23 implements circle, square {
-    public void area(int a, int b) {
-        System.out.println(a * b);
-    } // Area of square
-
-    public void area(int a) {
-        System.out.println(3.14 * a);
-    } // Area of circle
-
     public static void main(String[] args) {
         p23 pObj = new p23();
-        pObj.area(l, m);
-        pObj.area(r);
+        pObj.print(2);
+        pObj.print(5,5);
     }
 }
