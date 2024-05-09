cc5bc2d559699af72879eccc6e0e64962f8ac8c0
Nimesh <nimeshkadecha4560@gmail.com>
2024-05-09 11:13:41 +0530

    adding p25

--- Programs/p25.java
+++ Programs/p25.java
@@ -3,19 +3,19 @@ write a program that illustrates interface inheritance interface p is extended b
 then interface p12 inherits from both p1 and p2 each interface declares 1 constant and 1 method.
 class q implements p12 instantiate q and invoke each method. each method displays one of the constants.
 */
-interface p{ int a = 10; void p(); }
-interface p1 extends p{ int b = 20; void p1(); }
-interface p2 extends p{ int c = 30; void p2(); }
-interface p12 extends p1,p2{ int d = 40; void p3(); }
-class Q implements p12{
-     public void p3() { System.out.println(a); }
-     public void p1() { System.out.println(b); }
-     public void p2() { System.out.println(c); }
-     public void p() { System.out.println(d); }
+interface p { int a = 10; void p(); }
+interface p1_1 extends p { int b = 20; void p11(); }
+interface p2 extends p { int c = 30; void p22(); }
+interface p12 extends p1_1, p2 { int d = 40; void p33(); }
+class q implements p12 {
+    public void p33() { System.out.println(a); }
+    public void p22() { System.out.println(c); }
+    public void p11() { System.out.println(b); }
+    public void p() { System.out.println(d); }
 }
 public class p25 {
-     public static void main(String[] args) {
-          Q q = new Q();
-          q.p(); q.p1(); q.p2(); q.p3();
-     }
+    public static void main(String[] args) {
+        q Q = new q();
+        Q.p(); Q.p11(); Q.p22(); Q.p33();
+    }
 }
