8747cd558c44763bb871f0e7bb82f69c2eca9925
Nimesh <nimeshkadecha4560@gmail.com>
2024-05-19 12:01:43 +0530

    Updating min_max

--- Programs/Min_Max.java
+++ Programs/Min_Max.java
@@ -1,42 +1,10 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Min_Max {
-    public static int min(int[] arr){
-        int min=0;
-        for(int i=0;i< arr.length-1;i++){
-            min = i;
-            if(arr[min]>arr[i+1]){
-                min = arr[i+1];
-            }else {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
-    public static int max(int[] arr){
-        int max=0;
-        for(int i=0;i< arr.length-1;i++){
-            max = i;
-            if(arr[max]<arr[i+1]){
-                max = arr[i+1];
-            }else {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Size of Array : ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter "+i+" number :");
-            arr[i]= sc.nextInt();
-        }
-        int min = min(arr);
-        System.out.println("Maximum Element IS :"+min);
-        int max = max(arr);
-        System.out.println("Maximum Element IS :"+max);
-    }
+    static int len = 5;
+    static int [] arr = {9,5,1,3,7} ;
+    static void print(int num){ System.out.println(num); }
+    static void min(){ print(Arrays.stream(arr).min().getAsInt()); }
+    static void max(){ print(Arrays.stream(arr).max().getAsInt()); }
+    public static void main(String[] args){ min(); max(); }
 }
